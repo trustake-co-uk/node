610d85073256149b56911c08ef76a4e124c1021b
Hunter <thecryptohunter@protonmail.com>
2020-01-14 16:13:22 +0000

    update rcp function

--- include/config.php
+++ include/config.php
@@ -11,7 +11,7 @@ $email='';
 // general variables
 $server_ip='localhost'; // '0.0.0.0' target server ip. [ex.] 10.0.0.15
 $scheme='http' ;// tcp protocol to access json on coin. [default]
-$AccountName='coldStakingHotAddresses' ; // special account for cold staking addresses
+$AccountName='coldStakingHotAddresses' ; // special account for cold staking addresses (deprecated for pivx forks)
 $api_ver=''; // additional parameters for api call (deprecated for pivx forks) 
 $coldstakeui=''; // switch off the scripts (deprecated for pivx forks) 
 $whitelist=''; //is whitelisting enabled (deprecated for pivx forks) 
--- include/functions.php
+++ include/functions.php
@@ -32,39 +32,29 @@ class phpFunctions_Wallet
         $rpcuser=$WalletName;
         $url = $scheme . '://' . $server_ip . ':' . $api_port . '/';
         $request = '{"jsonrpc": "1.0", "$rpcuser":"$rpcpass", "method": "' . $command . '", "params": [' . $params . '] }';
-        
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
         curl_setopt($ch, CURLOPT_USERPWD, "$rpcuser:$rpcpass");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: text/plain'));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "accept: application/json",
+            "content-type: application/json-patch+json",
+        ));
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+        
         $response = curl_exec($ch);
         $response = json_decode($response, true);
-        $result = $response['result'];
-        $error  = $response['error'];
-        curl_close($ch);
+        $result=$response['result'];
+        $error=$response['error'];
 
-        switch ($command) {
-
-            case "validateaddress":
-                return ($result['isvalid']);
-                break;
-            case "walletpassphrase":
-                if (empty($result) && empty($error)) {
-                    return true;
-                } else {
-                    return '<span class="ui-state-error" style="padding-left:2px;">' . nl2br(htmlentities($response['error']['message'])) . '</span>';
-                }
-                break;
-            default:
-                if (!is_null($error)) {
-                    return $response['error']['message'];
-                } else {
-                    return $result;
-                }
+        if ( isset($error) ) {
+            return $error;
+        } else {
+            return $result;
         }
+        curl_close($ch);
     }
 
     public function CallAPI($url, $request_type)
