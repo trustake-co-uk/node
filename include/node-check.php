5b49471ee6c105f5c170219be0b951c2dc3863e2
Hunter <thecryptohunter@protonmail.com>
2020-01-16 16:20:42 +0000

    enable staking status

--- include/node-check.php
+++ include/node-check.php
@@ -14,24 +14,18 @@ if (is_array($get_stakinginfo)) {
 	$enabled = <<<EOD
 <a href="index.html" class="icon fa-circle" style="color:green">COLDSTAKE.CO.IN</a>
 EOD;
-	$message = <<<EOD
-<li><a href=""class="icon fa-circle" style='color:green'>Node online</a></li>
-EOD;
-
-
-	// 	// Check staking status and set messages 		
-	// 	if ($get_stakinginfo['staking status'] == 1) {
-	// 		// Staking online
-	// 		$message = <<<EOD
-	// <li><a href=""class="icon fa-circle" style='color:green'>Staking online</a></li>
-	// EOD;
-	// 	} else {
-	// 		// Staking offline
-	// 		$message = <<<EOD
-	// <li><a href=""class="icon fa-circle" style='color:red'>Staking offline</a></li>
-	// EOD;
-	// 	}
-
+	// Check staking status and set messages 		
+	if ($get_stakinginfo['staking_enabled'] == 1) {
+		// Staking online
+		$message = <<<EOD
+	<li><a href=""class="icon fa-circle" style='color:green'>Staking online</a></li>
+	EOD;
+	} else {
+		// Staking offline
+		$message = <<<EOD
+	<li><a href=""class="icon fa-circle" style='color:red'>Staking offline</a></li>
+	EOD;
+	}
 } else {
 	$message = <<<EOD
 <li><a href=""class="icon fa-circle" style='color:red'>Node offline</a></li>
